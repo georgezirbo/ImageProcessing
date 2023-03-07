02c1b4d383447dae34a1191622413afb1aa23887
samzirbo <83039189+samzirbo@users.noreply.github.com>
2023-03-07 15:29:00 +0200

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,20 +2,7 @@
 #define _CRTDBG_MAP_ALLOC
 #include <stdlib.h>
 #include <crtdbg.h>
-
-#include <iostream>
-#include <iomanip>
-#include "tests\Tests.h"
-#include "domain\Image\Image.h"
-#include "domain\ImageProcessing\ImageProcessing.h"
-#include "domain\ImageProcessing\BrightnessContrast\Brightness_Contrast.h"
-#include "domain\ImageProcessing\GammaCorrection\Gamma.h"
-#include "domain\ImageProcessing\Convolution\Convolution.h"
-#include "utils\Draw.h"
 #include "ui\Console.h"
-using std::cout;
-using std::cin;
-using std::endl;
 
 int main()
 {
